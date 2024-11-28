fcc611a8408a4b141ed107c9237af3559be87791
Chong Jin <ottstormtrooper@gmail.com>
2024-11-28 16:16:13 -0500

    main page

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   devServer: {
     host: 'localhost', // Force the server to use localhost
     port: 8080,
   },
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  publicPath: '/',
+  outputDir: 'dist'
 })
